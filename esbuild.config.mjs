cceac05338f4569f1d6dc333255c015507af51c7
Theodore Phan <7644428+pttri@users.noreply.github.com>
2025-06-29 20:47:47 +0700

    Build: Add `dist` folder

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,7 @@
 import esbuild from "esbuild";
 import process from "process";
 import builtins from "builtin-modules";
+import { copy } from 'esbuild-plugin-copy';
 
 const banner =
 `/*
@@ -15,7 +16,7 @@ const context = await esbuild.context({
 	banner: {
 		js: banner,
 	},
-	entryPoints: ["main.ts"],
+	entryPoints: ["./src/main.ts"],
 	bundle: true,
 	external: [
 		"obsidian",
@@ -37,7 +38,19 @@ const context = await esbuild.context({
 	logLevel: "info",
 	sourcemap: prod ? false : "inline",
 	treeShaking: true,
-	outfile: "main.js",
+	outdir: "dist",
+	plugins: [
+		copy({
+			// this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
+			// if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
+			resolveFrom: 'cwd',
+			assets: {
+			from: ['./manifest.json'],
+			to: ['./dist/manifest.json'],
+			},
+			watch: true,
+		}),
+	],
 });
 
 if (prod) {
